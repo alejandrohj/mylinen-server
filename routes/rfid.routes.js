b16f626b4e2091d74ec16fc2ccbf591614f1c810
Alejandro Hernández <alejandro_hj.11@outlook.com>
2021-06-01 16:33:54 +0100

    stock handleded by date and washes request done

--- routes/rfid.routes.js
+++ routes/rfid.routes.js
@@ -222,7 +222,7 @@ router.get(`/customer/:id/stock`,(req,res) =>{
   })
 })
 router.get(`/customer/:id/stock/bydate`,(req,res) =>{
-  req.setTimeout(5000000);
+  res.setTimeout(5000000);
   let tagsByDateArr = [];
   let tagsByDateReducedArr = [];
   const id = req.params.id;
